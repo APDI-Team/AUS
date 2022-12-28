660cc7ab49a9cc9657495df732123222b25c278e
clydelhj <hleebg@connect.ust.hk>
2022-12-29 00:00:32 +0800

    Profile page

--- lib/views/profile_page.dart
+++ lib/views/profile_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key, required this.title}) : super(key: key);
-
   // This class is the configuration for the state. It holds the values (in this
   // case the title) provided by the parent (in this case the App widget) and
   // used by the build method of the State. Fields in a Widget subclass are
@@ -18,14 +17,57 @@ class ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(widget.title),
-          ],
+            body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Padding(
+                padding: const EdgeInsetsDirectional.fromSTEB(0, 30, 0, 20),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: const [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                        child: Text(
+                          'Welcome, USER NAME',
+                          style:
+                              TextStyle(
+                                    fontFamily: 'Roboto',
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.max,
+                children: const [
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+                      child: Text(
+                        'USER UNIVERSITY',
+                        style: TextStyle(
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w100,
+                                color: Colors.grey
+                            ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
-    );
+
+	);
   }
 }
